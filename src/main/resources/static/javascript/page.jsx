128b51103c407691f5b1b577e136df83b1c99c1f
Andy Cate <and164v@gmail.com>
2019-01-11 09:48:03 -0800

    Set up widget framework

--- src/main/resources/static/javascript/page.jsx
+++ src/main/resources/static/javascript/page.jsx
@@ -1,33 +1,45 @@
 class WidgetContainer extends React.Component {
     render() {
-        // we need IDs for Bootstrap
         return (
-            <div class="card m-1" style="width: {this.props.width}; height: {this.props.height};" id="{this.props.id}_card">
-                <div class="card-header p-1">
-                    <h4 class="m-0 d-inline">{this.props.title}</h4>
-                    <a class="btn btn-light float-right d-inline p-1" role="button" data-toggle="collapse" href=".{this.props.id}_collapse"><h5 class="fas fa-cog m-0"></h5></a>
+            <div className='card m-1' style={{width: this.props.width, height: this.props.height, display:'inline-block'}} id={this.props.id + '_card'}>
+                <div className='card-header p-1'>
+                    <h4 className='m-0 d-inline'>{this.props.title}</h4>
+                    <a className='btn btn-light float-right d-inline p-0 m-1' role='button' data-toggle='collapse' href={'.' + this.props.id + '_collapse'}><h5 className='fas fa-cog m-0'></h5></a>
                 </div>
-                <div class="card-body p-2 collapse show {this.props.id}_collapse" id="{this.props.id}_widget">
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
-                Main widget
+class WidgetBody extends React.Component {
+    render() {
+        return (
+            <div className={'card-body p-2 collapse show ' + this.props.id + '_collapse'} id={this.props.id + '_widget'}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
 
-                </div>
-                <div class="card-body p-2 collapse {this.props.id}_collapse" id="{this.props.id}_settings">
-
-                Settings for widget
-
-                </div>
+class WidgetSettings extends React.Component {
+    render() {
+        return (
+            <div className={'card-body p-2 collapse ' + this.props.id + '_collapse'} id={this.props.id + '_settings'}>
+                {this.props.children}
             </div>
         );
     }
 }
 
 
+
+
 var config = {}
 
 $(function() { // runs when document finishes loading
-    $.getJSON( "/config", function( data ) {
-        console.log("config:")
+    $.getJSON( '/config', function( data ) {
+        console.log('config:')
         console.log(data)
         config = data
     }).done(function () {
@@ -39,17 +51,29 @@ $(function() { // runs when document finishes loading
     });
 
     ReactDOM.render(
-        <WidgetContainer title="Test Variable" width="12rem" height="auto" id="testVarEditor"/>,
-        $("#reactapp")[ 0 ]
+        <div>
+            <RawVarEditor title='Editor 1' id='editor1' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 2' id='editor2' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 3' id='editor3' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 4' id='editor4' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 5' id='editor5' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 6' id='editor6' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 7' id='editor7' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 8' id='editor8' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 9' id='editor9' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 10' id='editor10' width='12rem' height='auto' />
+            <RawVarEditor title='Editor 11' id='editor11' width='12rem' height='auto' />
+        </div>,
+        $('#reactapp')[ 0 ]
     );
 });
 
 class PageUtils {
     static getPageJSON(json, page) {
-        return json["pages"][page]
+        return json['pages'][page]
     }
 
     static getPageWidgets(json, page) {
-        return getPageJSON(json, page)["widgets"]
+        return getPageJSON(json, page)['widgets']
     }
 }
--- src/main/resources/static/javascript/widgets/rawvareditor.jsx
+++ src/main/resources/static/javascript/widgets/rawvareditor.jsx
@@ -1,9 +1,16 @@
-class RawVarEditor {
-    constructor(jsonObject) {
-        this.jsonObject = jsonObject
-
-        this.template = <div>
-
-        </div>;
+class RawVarEditor extends React.Component {
+    render() {
+        return (
+            <WidgetContainer title={this.props.title} width={this.props.width} height={this.props.height} id={this.props.id}>
+                <WidgetBody id={this.props.id}>
+                    <input className='form-control mb-2' type='text' id={this.props.id + '_var_display'} placeholder='value' />
+                    <button className='btn btn-primary' id={this.props.id + '_body_submit'}>Submit</button>
+                </WidgetBody>
+                <WidgetSettings id={this.props.id}>
+                    <input className='form-control mb-2' type='text' name='TEST' defaultValue='test value' />
+                    <button className='btn btn-primary' id={this.props.id + '_settings_submit'}>Submit</button>
+                </WidgetSettings>
+            </WidgetContainer>
+        );
     }
 }
