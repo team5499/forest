f8f8e6adbee6587052098e4ebee19f598a98ce72
Andy Cate <and164v@gmail.com>
2019-01-11 21:02:09 -0800

    Add gridstack, new settings interface and dynamic loading of Widgets

--- src/main/resources/static/javascript/page.jsx
+++ src/main/resources/static/javascript/page.jsx
@@ -4,7 +4,7 @@ class WidgetContainer extends React.Component {
             <div className='card m-1' style={{width: this.props.width, height: this.props.height, display:'inline-block'}} id={this.props.id + '_card'}>
                 <div className='card-header p-1'>
                     <h4 className='m-0 d-inline'>{this.props.title}</h4>
-                    <a className='btn btn-light float-right d-inline p-0 m-1' role='button' data-toggle='collapse' href={'.' + this.props.id + '_collapse'}><h5 className='fas fa-cog m-0'></h5></a>
+                    <button className='btn btn-light float-right d-inline p-0 m-1' type='button' data-toggle='modal' data-target={'#' + this.props.id + '_modal'}><h5 className='fas fa-cog m-0'></h5></button>
                 </div>
                 {this.props.children}
             </div>
@@ -15,7 +15,7 @@ class WidgetContainer extends React.Component {
 class WidgetBody extends React.Component {
     render() {
         return (
-            <div className={'card-body p-2 collapse show ' + this.props.id + '_collapse'} id={this.props.id + '_widget'}>
+            <div className={'card-body p-2 show'} id={this.props.id + '_widget'}>
                 {this.props.children}
             </div>
         );
@@ -25,8 +25,24 @@ class WidgetBody extends React.Component {
 class WidgetSettings extends React.Component {
     render() {
         return (
-            <div className={'card-body p-2 collapse ' + this.props.id + '_collapse'} id={this.props.id + '_settings'}>
-                {this.props.children}
+            <div className='modal fade' id={this.props.id + '_modal'} tabIndex='-1' role='dialog' aria-hidden='true'>
+                <div className='modal-dialog modal-dialog-centered' role='document'>
+                    <div className='modal-content'>
+                        <div className='modal-header'>
+                            <h5 className='modal-title' id={this.props.id + '_modal_title'}>{this.props.title} Settings</h5>
+                            <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span>
+                            </button>
+                        </div>
+                        <div className='modal-body'>
+                            {this.props.children}
+                        </div>
+                        <div className='modal-footer'>
+                            <button type='button' className='btn btn-secondary' data-dismiss='modal'>Close</button>
+                            <button type='button' className='btn btn-primary'>Save changes</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -35,45 +51,67 @@ class WidgetSettings extends React.Component {
 
 
 
-var config = {}
+var config = {};
+var pagename = '';
 
 $(function() { // runs when document finishes loading
+    let pathname = window.location.pathname;
+    pagename = pathname.substring(pathname.lastIndexOf('/') + 1, pathname.length);
+    console.log(pagename);
+
     $.getJSON( '/config', function( data ) {
-        console.log('config:')
-        console.log(data)
-        config = data
+        console.log('config:');
+        console.log(data);
+        config = data;
+        let TestElement = 'WidgetContainer';
+        console.log(TestElement);
+        ReactDOM.render(
+            <div>
+                {PageUtils.renderWidgets(config, pagename)}
+            </div>,
+            $('#reactapp')[ 0 ]
+        );
     }).done(function () {
-
-    }).fail(function () {
-        // figure out how to deal with JSON request failing
+    }).fail(function (jqxhr, textStatus, error) {
+        // display error on page
+        let err = textStatus + ', ' + error;
+        ReactDOM.render(
+            <div>
+                <div class='alert alert-danger p-2 show' role='alert'>
+                    There was an error loading the JSON config from the robot:
+                    <br />
+                    <b>{err}</b>
+                </div>
+            </div>,
+            $('#reactapp')[ 0 ]);
     }).always(function () {
-        // leave this here, just in case its useful later
     });
-
-    ReactDOM.render(
-        <div>
-            <RawVarEditor title='Editor 1' id='editor1' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 2' id='editor2' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 3' id='editor3' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 4' id='editor4' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 5' id='editor5' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 6' id='editor6' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 7' id='editor7' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 8' id='editor8' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 9' id='editor9' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 10' id='editor10' width='12rem' height='auto' />
-            <RawVarEditor title='Editor 11' id='editor11' width='12rem' height='auto' />
-        </div>,
-        $('#reactapp')[ 0 ]
-    );
 });
 
 class PageUtils {
+    static WidgetClasses = {};
+
     static getPageJSON(json, page) {
-        return json['pages'][page]
+        return json['pages'][page];
     }
 
     static getPageWidgets(json, page) {
-        return getPageJSON(json, page)['widgets']
+        return PageUtils.getPageJSON(json, page)['widgets'];
+    }
+
+    static getWidgetTag(widget) {
+        return `${widget.type}`;
+    }
+
+    static renderWidgets(json, page) {
+        let widgetsJson = PageUtils.getPageWidgets(json, page);
+        let widgets = [];
+        for(var i in widgetsJson) {
+            let widget = widgetsJson[i];
+            const GenericWidget = PageUtils.getWidgetTag(widget);
+            widgets.push(React.createElement(PageUtils.WidgetClasses[GenericWidget], {key: widget.id, title: widget.title, id: widget.id, width: widget.width, height: widget.height, variables: widget.variables, kwargs: widget.kwargs}, null));
+            //widgets.push(<GenericWidget key={i.id} title={i.title} id={i.id} width={i.width} height={i.height} variables={i.variables} kwargs={i.kwargs} />);
+        }
+        return widgets;
     }
 }
--- src/main/resources/static/javascript/widgets/rawvareditor.jsx
+++ src/main/resources/static/javascript/widgets/rawvareditor.jsx
@@ -2,15 +2,17 @@ class RawVarEditor extends React.Component {
     render() {
         return (
             <WidgetContainer title={this.props.title} width={this.props.width} height={this.props.height} id={this.props.id}>
-                <WidgetBody id={this.props.id}>
+                <WidgetBody title={this.props.title} id={this.props.id}>
                     <input className='form-control mb-2' type='text' id={this.props.id + '_var_display'} placeholder='value' />
                     <button className='btn btn-primary' id={this.props.id + '_body_submit'}>Submit</button>
                 </WidgetBody>
-                <WidgetSettings id={this.props.id}>
+                <WidgetSettings title={this.props.title} id={this.props.id}>
                     <input className='form-control mb-2' type='text' name='TEST' defaultValue='test value' />
-                    <button className='btn btn-primary' id={this.props.id + '_settings_submit'}>Submit</button>
                 </WidgetSettings>
             </WidgetContainer>
         );
     }
 }
+
+// make sure to do this for every widget
+PageUtils.WidgetClasses['RawVarEditor'] = RawVarEditor
